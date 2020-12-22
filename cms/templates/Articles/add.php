79a2081238c008e7421f56cfa335274bfe5801cb
kurocraft0312 <kurocraft0312@gmail.com>
2020-12-22 09:00:40 +0900

    add:シンプルなスラッグ作成の追加

---
+++ cms/templates/Articles/add.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CakePHPの練習3</title>
+</head>
+<body>
+    <!-- File: templates/Articles/add.php -->
+    <h1>記事の追加</h1>
+    <?php
+        echo $this->Form->create($article);
+        // 今はユーザーを直接記述
+        echo $this->Form->control('user_id', ['type' => 'hidden', 'value' => 1]);
+        echo $this->Form->control('title');
+        echo $this->Form->control('body', ['rows' => '3']);
+        echo $this->Form->button(__('Save Article'));
+        echo $this->Form->end();
+    ?>
+</body>
+</html>
