05b1b5ab807eb60f827f0b80bc678b562121f014
kurocraft0312 <kurocraft0312@gmail.com>
2020-12-23 01:10:18 +0900

    add:パスワードハッシュ化の追加（ここまで正常）

--- cms/tests/Fixture/UsersFixture.php
+++ cms/tests/Fixture/UsersFixture.php
@@ -43,8 +43,8 @@ class UsersFixture extends TestFixture
                 'id' => 1,
                 'email' => 'Lorem ipsum dolor sit amet',
                 'password' => 'Lorem ipsum dolor sit amet',
-                'created' => '2020-12-22 15:30:52',
-                'modified' => '2020-12-22 15:30:52',
+                'created' => '2020-12-22 16:03:10',
+                'modified' => '2020-12-22 16:03:10',
             ],
         ];
         parent::init();
